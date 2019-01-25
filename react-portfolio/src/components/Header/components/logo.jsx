28e44914ef6d246ef97285ec5ce782fc9fa96655
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-01-25 18:29:02 -0500

    UPDATE: adding styles to the icon

--- react-portfolio/src/components/Header/components/logo.jsx
+++ react-portfolio/src/components/Header/components/logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Icon from '../../../assets/resources/David.png'
+import './logo.scss'
 
 function Logo(props){
     return(
-        <div>
-            <img src={Icon} alt="Logo of the page"/>
-            <h3>David</h3>
+        <div className="Logo">
+            <img src={Icon} width={35} height={35} alt="Logo of the page"/>
+            <h3 className="Logo-title"><strong>David</strong>Ruiz</h3>
         </div>
     )
 }
