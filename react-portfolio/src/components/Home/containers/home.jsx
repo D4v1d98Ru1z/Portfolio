76610b67e102b09509765e2c4955ccaf53e6d7e5
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-01-24 19:59:21 -0500

    UPDATE: home

--- react-portfolio/src/components/Home/containers/home.jsx
+++ react-portfolio/src/components/Home/containers/home.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import HomeLayout from '../components/home-layout'
+import Header from '../../Header/containers/header'
 
 class Home extends Component {
     render(){
         return(
-            <HomeLayout/>
+            <HomeLayout>
+                <Header/>
+            </HomeLayout>
         )
     }
 }
