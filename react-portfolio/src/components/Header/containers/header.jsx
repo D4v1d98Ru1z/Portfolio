f152477a810384d5fc6985eb96997b8248997c14
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-01-24 20:26:01 -0500

    UPDATE: mapping the data

--- react-portfolio/src/components/Header/containers/header.jsx
+++ react-portfolio/src/components/Header/containers/header.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 import HeaderLayout from '../components/header-layout'
 import Logo from '../components/logo'
+import Links from '../components/links'
 
 class Header extends Component {
     render() {
         return(
             <HeaderLayout>
                 <Logo/>
+                <div>
+                    {
+                        this.props.navbar.map(item => {
+                            return(
+                                <Links
+                                    key={item.id}
+                                    {...item}
+                                />
+                            )
+                        })
+                    }
+                </div>
+
             </HeaderLayout>
         )
     }
