93fe68279325b8fa5ec058d5393ae140b8381723
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-01-24 19:58:29 -0500

    ADD: logo component

---
+++ react-portfolio/src/components/Header/components/logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Icon from '../../../assets/resources/David.png'
+
+function Logo(props){
+    return(
+        <div>
+            <img src={Icon} alt="Logo of the page"/>
+            <h3>David</h3>
+        </div>
+    )
+}
+
+export default Logo
